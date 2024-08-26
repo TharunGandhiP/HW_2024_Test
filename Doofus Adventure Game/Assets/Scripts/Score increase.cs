546076fef0a82ffd15061f2d9851787b79496a8a
iiTzGandhi <tharungandhi.perasiriyan@gmail.com>
2024-08-26 21:03:21 +0530

    Level 2

---
+++ Doofus Adventure Game/Assets/Scripts/Score increase.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Scoreincrease : MonoBehaviour
+{
+    int score = 0;
+    [SerializeField] Text Scoring;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("ScoreObject"))
+        {
+            score++;
+            Scoring.text = "Score: " + score;
+            if(score>PlayerPrefs.GetInt("HighScore",0))
+            {
+                PlayerPrefs.SetInt("HighScore", score);
+            }
+            if (score == 50)
+            {
+                SceneManager.LoadScene(3);
+                score = 50;
+            }
+        }
+    }
+}
